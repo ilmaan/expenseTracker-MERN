84d56164e6f2b2a46b82db6327773d3b19feeb8e
unknown <ilmancc@gmail.com>
2024-10-31 14:39:49 -0700

    Expense Tracker basic layout

---
+++ backend/typeDefs/transaction.typeDef.js
@@ -0,0 +1,46 @@
+const transactionTypeDef = `#graphql
+    type Transaction {
+        _id: ID!
+        userId: ID!
+        description: String!
+        amount: Float!
+        category: String!
+        location: String
+        date: String!
+    }
+
+    type Query{
+        transactions: [Transaction!]!
+        transaction(transactionID: ID!): Transaction
+    }
+
+    type Mutation {
+        createTransaction(input: createTransactionInput!): Transaction
+        updateTransaction(input: updateTransactionInput!): Transaction
+        deleteTransaction(transactionID: ID!): Transaction!
+    }
+
+    input createTransactionInput{
+        description: String!
+        amount: Float!
+        category: String!
+        location: String
+        date: String!
+    }
+
+    input updateTransactionInput{
+        transactionID: ID!
+        userId: ID
+        description: String
+        amount: Float
+        category: String
+        location: String
+        date: String
+    }
+
+
+
+
+`;
+
+export default transactionTypeDef;
