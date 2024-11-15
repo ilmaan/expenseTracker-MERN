35d7b1912fce0eefd4bfb47e160beef10a4c45b0
unknown <ilmancc@gmail.com>
2024-11-15 10:31:00 -0800

    Minor changes for deployment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,9 +14,9 @@ export default {
 			matchUtilities(
 				{
 					"bg-grid": (value) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-						)}")`,
+						backgroundImage: {
+							'grid': `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.05'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`
+						}
 					}),
 					"bg-grid-small": (value) => ({
 						backgroundImage: `url("${svgToDataUri(
